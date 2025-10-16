7eb2dc273ba192781fa380eca79406380096819b
bonlsls <whitewings358@gmail.com>
2025-10-17 01:03:13 +0700

    second commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="abc">
+      <div>
+        <Button>
+          I am a buton 
+        </Button>
+      </div>
+    </div>
+  );
+}
